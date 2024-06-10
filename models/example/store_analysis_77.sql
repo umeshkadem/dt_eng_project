d41ba0305e754d353a283f589fc1bc1553e31939
umeshkadem <kademumesh127@gmail.com>
2024-06-10 13:00:23 +0530

    initial

---
+++ models/example/store_analysis_77.sql
@@ -0,0 +1,304 @@
+
+-- models/sales_analysis.sql
+{{ config(enabled = true, materialized='table') }}
+
+
+-- first cte:
+with store_sales_1 as (
+    select ss_store_sk, ss_ext_sales_price, ss_net_profit, ss_sold_time_sk, ss_sold_date_sk
+     from {{ source('snowflake_sample_data', 'store_sales') }}
+), 
+date_dim_1 as (
+    select d_date_sk, 
+    d_date 
+    from {{ source('snowflake_sample_data', 'date_dim') }}
+),
+ss_join_d as (
+    select ss1.ss_sold_date_sk,
+            ss1.ss_store_sk,
+            d1.d_date_sk,
+            ss1.ss_ext_sales_price,
+            ss1.ss_net_profit
+
+    from 
+        store_sales_1 as ss1
+    join 
+        date_dim_1 as d1 on ss1.ss_sold_date_sk = d1.d_date_sk
+        
+    where d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+ss as (
+    select 
+        ss_join_d.ss_store_sk,
+        sum(ss_join_d.ss_ext_sales_price) as sales,
+        sum(ss_join_d.ss_net_profit) as profit
+    from 
+        ss_join_d
+    group by 
+        ss_join_d.ss_store_sk
+), 
+
+-- second cte
+store_returns_1 as (
+    select 
+        sr_store_sk, 
+        sr_return_amt, 
+        sr_net_loss, 
+        sr_returned_date_sk
+    from 
+        {{ source('snowflake_sample_data', 'store_returns') }}
+), 
+-- date_dim_1 as (
+--     select 
+--         d_date_sk, 
+--         d_date 
+--     from 
+--         {{ source('snowflake_sample_data', 'date_dim') }}
+-- ),
+sr_join_d as (
+    select 
+        sr1.sr_returned_date_sk,
+        sr1.sr_store_sk,
+        d1.d_date_sk,
+        sr1.sr_return_amt,
+        sr1.sr_net_loss
+    from 
+        store_returns_1 as sr1
+    join 
+        date_dim_1 as d1 on sr1.sr_returned_date_sk = d1.d_date_sk
+    where 
+        d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+sr as (
+    select 
+        sr_join_d.sr_store_sk,
+        sum(sr_join_d.sr_return_amt) as returns,
+        sum(sr_join_d.sr_net_loss) as profit_loss
+    from 
+        sr_join_d
+    group by 
+        sr_join_d.sr_store_sk
+),
+
+-- third cte 
+
+catalog_sales_1 as (
+    select 
+        cs_call_center_sk, 
+        cs_ext_sales_price, 
+        cs_net_profit, 
+        cs_sold_date_sk
+    from 
+        {{ source('snowflake_sample_data', 'catalog_sales') }}
+), 
+-- date_dim_1 as (
+--     select 
+--         d_date_sk, 
+--         d_date 
+--     from 
+--         {{ source('snowflake_sample_data', 'date_dim') }}
+-- ),
+cs_join_d as (
+    select 
+        cs1.cs_sold_date_sk,
+        cs1.cs_call_center_sk,
+        d1.d_date_sk,
+        cs1.cs_ext_sales_price,
+        cs1.cs_net_profit
+    from 
+        catalog_sales_1 as cs1
+    join 
+        date_dim_1 as d1 on cs1.cs_sold_date_sk = d1.d_date_sk
+    where 
+        d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+cs as (
+    select 
+        cs_join_d.cs_call_center_sk,
+        sum(cs_join_d.cs_ext_sales_price) as sales,
+        sum(cs_join_d.cs_net_profit) as profit
+    from 
+        cs_join_d
+    group by 
+        cs_join_d.cs_call_center_sk
+),
+
+-- fourth cte
+catalog_returns_1 as (
+    select 
+        cr_call_center_sk, 
+        cr_return_amount, 
+        cr_net_loss, 
+        cr_returned_date_sk
+    from 
+        {{ source('snowflake_sample_data', 'catalog_returns') }}
+), 
+-- date_dim_1 as (
+--     select 
+--         d_date_sk, 
+--         d_date 
+--     from 
+--         {{ source('snowflake_sample_data', 'date_dim') }}
+-- ),
+cr_join_d as (
+    select 
+        cr1.cr_returned_date_sk,
+        cr1.cr_call_center_sk,
+        d1.d_date_sk,
+        cr1.cr_return_amount,
+        cr1.cr_net_loss
+    from 
+        catalog_returns_1 as cr1
+    join 
+        date_dim_1 as d1 on cr1.cr_returned_date_sk = d1.d_date_sk
+    where 
+        d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+cr as (
+    select 
+        cr_join_d.cr_call_center_sk,
+        sum(cr_join_d.cr_return_amount) as returns,
+        sum(cr_join_d.cr_net_loss) as profit_loss
+    from 
+        cr_join_d
+    group by 
+        cr_join_d.cr_call_center_sk
+),
+-- fifth cte
+
+web_sales_1 as (
+    select 
+        ws_web_page_sk, 
+        ws_ext_sales_price, 
+        ws_net_profit, 
+        ws_sold_date_sk
+    from 
+        {{ source('snowflake_sample_data', 'web_sales') }}
+), 
+-- date_dim_1 as (
+--     select 
+--         d_date_sk, 
+--         d_date 
+--     from 
+--         {{ source('snowflake_sample_data', 'date_dim') }}
+-- ),
+ws_join_d as (
+    select 
+        ws1.ws_sold_date_sk,
+        ws1.ws_web_page_sk,
+        d1.d_date_sk,
+        ws1.ws_ext_sales_price,
+        ws1.ws_net_profit
+    from 
+        web_sales_1 as ws1
+    join 
+        date_dim_1 as d1 on ws1.ws_sold_date_sk = d1.d_date_sk
+    where 
+        d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+ws as (
+    select 
+        ws_join_d.ws_web_page_sk,
+        sum(ws_join_d.ws_ext_sales_price) as sales,
+        sum(ws_join_d.ws_net_profit) as profit
+    from 
+        ws_join_d
+    group by 
+        ws_join_d.ws_web_page_sk
+),
+
+-- six cte
+web_returns_1 as (
+    select 
+        wr_web_page_sk, 
+        wr_return_amt, 
+        wr_net_loss, 
+        wr_returned_date_sk
+    from 
+        {{ source('snowflake_sample_data', 'web_returns') }}
+), 
+-- date_dim_1 as (
+--     select 
+--         d_date_sk, 
+--         d_date 
+--     from 
+--         {{ source('snowflake_sample_data', 'date_dim') }}
+-- ),
+wr_join_d as (
+    select 
+        wr1.wr_returned_date_sk,
+        wr1.wr_web_page_sk,
+        d1.d_date_sk,
+        wr1.wr_return_amt,
+        wr1.wr_net_loss
+    from 
+        web_returns_1 as wr1
+    join 
+        date_dim_1 as d1 on wr1.wr_returned_date_sk = d1.d_date_sk
+    where 
+        d1.d_date between cast('2000-08-23' as date) and (cast('2000-08-23' as date) + 30)
+),
+wr as (
+    select 
+        wr_join_d.wr_web_page_sk,
+        sum(wr_join_d.wr_return_amt) as returns,
+        sum(wr_join_d.wr_net_loss) as profit_loss
+    from 
+        wr_join_d
+    group by 
+        wr_join_d.wr_web_page_sk
+),
+
+-- final_cte
+
+fc as (
+    select 
+        'store channel' as channel,
+        ss.ss_store_sk as id,
+        sales,
+        coalesce(returns, 0) as returns,
+        (profit - coalesce(profit_loss, 0)) as profit
+    from 
+        ss 
+    left join 
+        sr on ss.ss_store_sk = sr.sr_store_sk
+
+    union all
+
+    select 
+        'catalog channel' as channel,
+        cs.cs_call_center_sk as id,
+        cs.sales,
+        coalesce(cr.returns, 0) as returns,
+        (cs.profit - coalesce(cr.profit_loss, 0)) as profit
+    from 
+        cs 
+    left join 
+        cr on cs.cs_call_center_sk = cr.cr_call_center_sk
+
+    union all
+
+    select 
+        'web channel' as channel,
+        ws.ws_web_page_sk as id,
+        ws.sales,
+        coalesce(wr.returns, 0) as returns,
+        (ws.profit - coalesce(wr.profit_loss, 0)) as profit
+    from 
+        ws 
+    left join 
+        wr on ws.ws_web_page_sk = wr.wr_web_page_sk
+)
+
+select 
+    channel,
+    id,
+    sum(sales) as sales,
+    sum(returns) as returns,
+    sum(profit) as profit
+from fc
+group by 
+    rollup(channel, id)
+order by 
+    channel,
+    id
---
+++ models/example/store_sales_by_time_88.sql
@@ -0,0 +1,199 @@
+-- models/store_sales_by_time.sql
+{{ config(enabled = true, materialized='table') }}
+
+
+with s_s as (
+    select ss_hdemo_sk, ss_sold_time_sk, ss_store_sk 
+    from {{ source('snowflake_sample_data', 'store_sales') }}
+),
+h_d as (
+    select hd_demo_sk, hd_dep_count, hd_vehicle_count 
+    from {{ source('snowflake_sample_data', 'household_demographics') }}
+),
+td as (
+    select t_time_sk, t_minute, t_hour 
+    from {{ source('snowflake_sample_data', 'time_dim') }}
+),
+st as (
+    select s_store_sk, s_store_name 
+    from {{ source('snowflake_sample_data', 'store') }}
+),
+
+-- first join
+ss_join_hd as (
+    select ss.ss_hdemo_sk, ss.ss_sold_time_sk, ss.ss_store_sk, hd.hd_demo_sk, hd.hd_dep_count, hd_vehicle_count 
+    from s_s as ss
+    join h_d as hd
+    on ss.ss_hdemo_sk = hd.hd_demo_sk 
+    and (
+        (hd.hd_dep_count = 4 and hd.hd_vehicle_count <= 4 + 2) or
+        (hd.hd_dep_count = 2 and hd.hd_vehicle_count <= 2 + 2) or
+        (hd.hd_dep_count = 0 and hd.hd_vehicle_count <= 0 + 2)
+    )
+),
+
+-- second join with unique names
+ss_hd_join_t1 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 8 and t.t_minute >= 30
+),
+ss_hd_join_t2 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 9 and t.t_minute < 30
+),
+ss_hd_join_t3 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 9 and t.t_minute >= 30
+),
+ss_hd_join_t4 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 10 and t.t_minute < 30
+),
+ss_hd_join_t5 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 10 and t.t_minute >= 30
+),
+ss_hd_join_t6 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 11 and t.t_minute < 30
+),
+ss_hd_join_t7 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 11 and t.t_minute >= 30
+),
+ss_hd_join_t8 as (
+    select ss_hd.ss_sold_time_sk, ss_hd.ss_store_sk, t.t_time_sk, t.t_hour, t.t_minute
+    from ss_join_hd as ss_hd 
+    join td as t 
+    on ss_hd.ss_sold_time_sk = t.t_time_sk
+    where t.t_hour = 12 and t.t_minute < 30
+),
+
+-- third join with unique names
+ss_hd_t_join_s1 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t1 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s2 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t2 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s3 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t3 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s4 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t4 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s5 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t5 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s6 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t6 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s7 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t7 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+ss_hd_t_join_s8 as (
+    select ss_hd_t.ss_store_sk, s.s_store_sk
+    from ss_hd_join_t8 as ss_hd_t
+    join st as s
+    on ss_hd_t.ss_store_sk = s.s_store_sk
+    where s.s_store_name = 'ese'
+),
+
+-- CTEs for counts
+s1 as (
+    select count(*) as h8_30_to_9
+    from ss_hd_t_join_s1
+),
+s2 as (
+    select count(*) as h9_to_9_30
+    from ss_hd_t_join_s2
+),
+s3 as (
+    select count(*) as h9_30_to_10
+    from ss_hd_t_join_s3
+),
+s4 as (
+    select count(*) as h10_to_10_30
+    from ss_hd_t_join_s4
+),
+s5 as (
+    select count(*) as h10_30_to_11
+    from ss_hd_t_join_s5
+),
+s6 as (
+    select count(*) as h11_to_11_30
+    from ss_hd_t_join_s6
+),
+s7 as (
+    select count(*) as h11_30_to_12
+    from ss_hd_t_join_s7
+),
+s8 as (
+    select count(*) as h12_to_12_30
+    from ss_hd_t_join_s8
+),
+
+-- final CTE
+fc as (
+    select 
+        s1.h8_30_to_9,
+        s2.h9_to_9_30,
+        s3.h9_30_to_10,
+        s4.h10_to_10_30,
+        s5.h10_30_to_11,
+        s6.h11_to_11_30,
+        s7.h11_30_to_12,
+        s8.h12_to_12_30
+    from s1, s2, s3, s4, s5, s6, s7, s8
+)
+
+select * from fc
